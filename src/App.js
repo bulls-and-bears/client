05791aa8b35cb68de6ba15b8a676b79180732ec6
hyeyeonismm <hyeyeonismm@gmail.com>
2023-10-11 17:40:15 +0900

    DSP-17 feature: 메인페이지 개발

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage.js';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+ body, html {
+        margin: 0;
+        padding: 0;
+        font-family: 'Noto Sans', sans-serif;
+    }
+
+`;
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<BrowserRouter>
+			<GlobalStyle />
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
