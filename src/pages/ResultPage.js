9e23b2d7ad09e0e626b48d76c61c2788c76a3551
hyeyeonismm <hyeyeonismm@gmail.com>
2023-10-13 22:49:30 +0900

    fix : 로딩 에러 수정

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -3,16 +3,18 @@ import React, { useEffect, useState } from 'react';
 import { Grid, styled, Button } from '@mui/material';
 import Header from '../components/Header';
 import ApexCharts from 'react-apexcharts';
-
+import { SyncLoader } from 'react-spinners';
 import axios from 'axios';
 
 function ResultPage() {
 	const [stockData, setStockData] = useState();
 	const [series, setSeries] = useState([]);
 	const [options, setOptions] = useState({});
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const fetchData = () => {
+			setLoading(true);
 			axios
 				.get('https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo', {
 					headers: {
@@ -53,9 +55,11 @@ function ResultPage() {
 							tickAmount: 11,
 						},
 					});
+					setLoading(false);
 				})
 				.catch((error) => {
 					console.log(error);
+					setLoading(false);
 				});
 		};
 		fetchData();
@@ -70,58 +74,65 @@ function ResultPage() {
 	return (
 		<>
 			<Header />
-
-			<Grid
-				sx={{
-					margin: '30px 140px',
-					display: 'flex',
-					flexDirection: 'row',
-					alignItems: 'center',
-					gap: 3,
-					justifyContent: 'space-around',
-				}}>
-				<Container sx={{ width: '397px', borderRadius: '40px', padding: '30px 40px' }}>
-					<Grid sx={{ fontSize: 20, fontWeight: 600, marginBottom: '25px' }}>나영현님의 포트폴리오</Grid>
-					<Grid sx={GridStyle}>
-						<div>보유 자산</div>
-						<div style={InputStyle}>100만원</div>
+			{loading ? (
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+					<SyncLoader color='#3f51b5' loading={loading} size={20} />
+				</div>
+			) : (
+				<>
+					<Grid
+						sx={{
+							margin: '30px 140px',
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							gap: 3,
+							justifyContent: 'space-around',
+						}}>
+						<Container sx={{ width: '397px', borderRadius: '40px', padding: '30px 40px' }}>
+							<Grid sx={{ fontSize: 20, fontWeight: 600, marginBottom: '25px' }}>나영현님의 포트폴리오</Grid>
+							<Grid sx={GridStyle}>
+								<div>보유 자산</div>
+								<div style={InputStyle}>100만원</div>
+							</Grid>
+							<Grid sx={GridStyle}>
+								<div>보유 기간</div>
+								<div style={InputStyle}>1년</div>
+							</Grid>
+						</Container>
+						<Container sx={{ width: '803px', borderRadius: '20px', padding: '20px' }}>
+							<Grid sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' }}>
+								<div style={{ fontWeight: 600, fontSize: 18 }}>추천 종목</div>
+								<Button sx={ButtonStyle}>더보기</Button>
+							</Grid>
+							<div style={{ maxHeight: '190px', overflowY: 'auto', marginTop: '8px' }}>
+								<table style={{ width: '100%', borderCollapse: 'collapse' }}>
+									<thead>
+										<tr>
+											<th style={TableStyle}>종목</th>
+											<th style={TableStyle}>현재가</th>
+											<th style={TableStyle}>등락률</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td style={{ borderBottom: '1px solid #eee', padding: '16px' }}>{stockData.itmsNm}</td>
+											<td style={{ borderBottom: '1px solid #eee', padding: '16px', textAlign: 'right' }}>{stockData.clpr}</td>
+											<td style={{ borderBottom: '1px solid #eee', padding: '16px', textAlign: 'right' }}>{stockData.fltRt}</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</Container>
 					</Grid>
-					<Grid sx={GridStyle}>
-						<div>보유 기간</div>
-						<div style={InputStyle}>1년</div>
+					<Grid sx={{ margin: '30px 140px' }}>
+						<div style={{ fontWeight: 600, fontSize: '20px', padding: '10px 20px' }}>주가 그래프</div>
+						<Container sx={{ borderRadius: '20px', height: '350px' }}>
+							<ApexCharts options={options} series={series} type='line' height={320} />
+						</Container>
 					</Grid>
-				</Container>
-				<Container sx={{ width: '803px', borderRadius: '20px', padding: '20px' }}>
-					<Grid sx={{ display: 'flex', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' }}>
-						<div style={{ fontWeight: 600, fontSize: 18 }}>추천 종목</div>
-						<Button sx={ButtonStyle}>더보기</Button>
-					</Grid>
-					<div style={{ maxHeight: '190px', overflowY: 'auto', marginTop: '8px' }}>
-						<table style={{ width: '100%', borderCollapse: 'collapse' }}>
-							<thead>
-								<tr>
-									<th style={TableStyle}>종목</th>
-									<th style={TableStyle}>현재가</th>
-									<th style={TableStyle}>등락률</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td style={{ borderBottom: '1px solid #eee', padding: '16px' }}>{stockData.itmsNm}</td>
-									<td style={{ borderBottom: '1px solid #eee', padding: '16px', textAlign: 'right' }}>{stockData.clpr}</td>
-									<td style={{ borderBottom: '1px solid #eee', padding: '16px', textAlign: 'right' }}>{stockData.fltRt}</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</Container>
-			</Grid>
-			<Grid sx={{ margin: '30px 140px' }}>
-				<div style={{ fontWeight: 600, fontSize: '20px', padding: '10px 20px' }}>주가 그래프</div>
-				<Container sx={{ borderRadius: '20px', height: '350px' }}>
-					<ApexCharts options={options} series={series} type='line' height={320} />
-				</Container>
-			</Grid>
+				</>
+			)}
 		</>
 	);
 }
